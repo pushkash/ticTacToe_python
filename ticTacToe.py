d148ea09dd541164addf9ff214d7e5a555692ed9
pushka <sm.pushkarev@gmail.com>
2017-04-02 01:00:35 +0300

    Added check of filled row

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,7 +1,21 @@
 import sys
-board = {'1': '1', '2': '2', '3': '3',
-	 '4': '4', '5': '5', '6': '6',
-	 '7': '7', '8': '8', '9': '9' }
+
+#possible options of win-rows
+#1-2-3
+#4-5-6
+#7-8-9
+
+#1-4-7
+#2-5-8
+#3-6-9
+
+#1-5-9
+#3-5-7
+
+
+board = {'1': 1, '2': 2, '3': 3,
+	     '4': 4, '5': 5, '6': 6,
+	 	 '7': 7, '8': 8, '9': 9 } 
 
 sign = ''
 
@@ -9,23 +23,23 @@ count = 0
 
 def boardDraw(board):
 	print '-' * 9
-	print board['1'] + ' | ' + board['2'] + ' | ' + board['3']
+	print str(board['1']) + ' | ' + str(board['2']) + ' | ' + str(board['3'])
 	print '-' * 9
-	print board['4'] + ' | ' + board['5'] + ' | ' + board['6']
+	print str(board['4']) + ' | ' + str(board['5']) + ' | ' + str(board['6'])
 	print '-' * 9
-	print board['7'] + ' | ' + board['8'] + ' | ' + board['9']
+	print str(board['7']) + ' | ' + str(board['8']) + ' | ' + str(board['9'])
 	print '-' * 9
 	print '\n' 
 
 def chooseSide():
 	while True:
-		sign = str(raw_input('Choose your sign (X or 0): '))
+		sign = str(raw_input('Choose your sign (X or 0, blank to quit): '))
 		sign = sign.upper()
 		print sign
 		if sign == '':
-			print 'Blank is exit, bye'
+			print 'Blank is exit'
 			sys.exit()
-		elif sign == 'X' or sign == 'x' or sign == '0':
+		elif sign == 'X' or sign == '0':
 			print 'Let it be'
 			break		
 		else:
@@ -36,15 +50,44 @@ def chooseSide():
 
 def fillSquare(sign, board):
 	while True:
-		square = raw_input('Choose a square number to put a ' + sign + " : ")
+		square = int(raw_input('Choose a square number to put a ' + sign + " : "))
 		if square not in board.values():
 			print 'Choose a square, please'
 			continue
 		else:
+			square = str(square)
 			board[square] = sign
 			break
 	return board
 
+
+def checkFullRow(board):
+
+	if board['1'] == board['2'] and board['2'] == board['3']:
+		print board['1'] == board['2'] == board['3']
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['4'] == board['5'] and board['5'] == board['6']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['7'] == board['8'] and board['8'] == board['9']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['1'] == board['4'] and board['4'] == board['7']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['2'] == board['5'] and board['5'] == board['8']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['3'] == board['6'] and board['6'] == board['9']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['1'] == board['5'] and board['5'] == board['9']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
+	elif board['3'] == board['5'] and board['5'] == board['7']:
+		print 'Sign ' + board['1'] + ' won'
+		sys.exit()
 
 
 sign = chooseSide()
@@ -53,17 +96,18 @@ boardDraw(board)
 
 while True:
 
+	checkFullRow(board)
+
 	if count == 9:
 		print "Game is over"
 		break
+
 
 	fillSquare(sign, board)
 
 	boardDraw(board)
 
 	count += 1
-
-
 
 
 
